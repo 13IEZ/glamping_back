ed74d82b7f8dd8fb471166cba1403a6b8040a28a
Pavel Khegay <pashulia.pk@gmail.com>
2021-06-19 13:59:37 +0600

    feature #2 changed the indent to 2

--- src/api/v1/models/Location.js
+++ src/api/v1/models/Location.js
@@ -3,56 +3,55 @@ const idValidator = require('mongoose-id-validator');
 const Schema = mongoose.Schema;
 
 const LocationSchema = new Schema({
-    title: {
-      type: String,
-      required: true,
-      unique: true
-    },
-    image: {
-      type: String,
-      required: true
-    },
-    square: {
-      type: Number,
-      required: true
-    },
-    description: {
-      type: String,
-      required: true
-    },
-    rent: {
-      type: Number,
-      required: true
-    },
-    status: {
-      type: Boolean,
-      default: true,
-      required: true
-    },
-    electrocity: {
-      type: Boolean,
-      default: true,
-      required: true
-    },
-    water: {
-      type: Boolean,
-      default: true,
-      required: true
-    },
-    road: {
-      type: Boolean,
-      default: true,
-      required: true
-    },
-    published: {
-      type: Boolean,
-      default: false,
-      required: true
-    },
-    owner: {
-      type: Schema.Types.ObjectId,
-      ref: 'User',
-      required: true
+  title: {
+    type: String,
+    required: true
+  },
+  image: {
+    type: String,
+    required: true
+  },
+  square: {
+    type: Number,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  rent: {
+    type: Number,
+    required: true
+  },
+  status: {
+    type: Boolean,
+    default: true,
+    required: true
+  },
+  electrocity: {
+    type: Boolean,
+    default: true,
+    required: true
+  },
+  water: {
+    type: Boolean,
+    default: true,
+    required: true
+  },
+  road: {
+    type: Boolean,
+    default: true,
+    required: true
+  },
+  published: {
+    type: Boolean,
+    default: false,
+    required: true
+  },
+  owner: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
   },
 });
 
