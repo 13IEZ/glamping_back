1c336c7ec00c3c685ab7f40f7dc7c02db54e16e8
Igor <zahik1311@gmail.com>
2021-06-27 17:58:20 +0600

    back-bug #29 improved User, users .js

--- src/api/v1/routes/users.js
+++ src/api/v1/routes/users.js
@@ -14,13 +14,65 @@ const createRouter = () => {
     }
   });
 
+  router.post('/', async (req, res) => {
+    try {
+      const user = new User(req.body);
+      user.generateToken();
+      await user.save();
+      res.send(user);
+    } catch (err) {
+      res.status(400).send(err);
+    }
+  });
+
+  router.post('/sessions', async (req, res) => {
+    try {
+      const user = await User.findOne({ email: req.body.email });
+
+      const errorMessage = 'Wrong username or password';
+
+      if (!user) return res.status(400).send({ error: errorMessage });
+
+      const isMatch = await user.checkPassword(req.body.password);
+
+      if (!isMatch) return res.status(400).send({ error: errorMessage });
+
+      user.generateToken();
+      try {
+        await user.save({ validateBeforeSave: false });
+      } catch (e) {
+        return res.status(500).send(e);
+      }
+      res.send(user);
+    } catch (e) {
+      res.status(400).send({ error: e.message });
+    }
+  });
+
+  router.delete('/sessions', async (req, res) => {
+    const token = req.get('Authentication');
+    const success = { message: 'Success' };
+
+    if (!token) return res.send(success);
+
+    const user = await User.findOne({ token });
+    if (!user) return res.send(success);
+
+    user.generateToken();
+    try {
+      await user.save({ validateBeforeSave: false });
+      return res.send(success);
+    } catch (e) {
+      res.status(500).send(e);
+    }
+  });
+
   router.get('/:id', auth, async (req, res) => {
     let user;
     try {
       user = await User.findById(req.params.id);
       res.send(user);
     } catch (error) {
-      console.log(error);
       res.status(500).send(error);
     }
   });
@@ -53,61 +105,6 @@ const createRouter = () => {
       res.send(updatedUser);
     } catch (error) {
       res.status(500).send(error);
-    }
-  });
-
-  router.post('/', async (req, res) => {
-    try {
-      const user = new User(req.body);
-      user.generateToken();
-      await user.save();
-      res.send(user);
-    } catch (err) {
-      res.status(400).send(err);
-    }
-  });
-
-  router.post('/sessions', async (req, res) => {
-    try {
-      const user = await User.findOne({ username: req.body.username });
-
-      const errorMessage = 'Wrong username or password';
-
-      if (!user) return res.status(400).send({ error: errorMessage });
-
-      const isMatch = await user.checkPassword(req.body.password);
-
-      if (!isMatch) return res.status(400).send({ error: errorMessage });
-
-      user.generateToken();
-
-      try {
-        await user.save();
-      } catch (e) {
-        return res.status(500).send(e);
-      }
-      res.send(user);
-    } catch (e) {
-      res.status(400).send({ error: e.message });
-    }
-  });
-
-  router.delete('/sessions', async (req, res) => {
-    const token = req.get('Authentication');
-    const success = { message: 'Success' };
-
-    if (!token) return res.send(success);
-
-    const user = await User.findOne({ token });
-
-    if (!user) return res.send(success);
-
-    user.generateToken();
-    try {
-      await user.save({ validateBeforeSave: false });
-      return res.send(success);
-    } catch (e) {
-      res.status(500).send(e);
     }
   });
 
