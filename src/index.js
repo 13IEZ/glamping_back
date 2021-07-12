92f90e2e4be1a131d8ef85f3fe7a0f5c1dc3820f
Mikhail Konev <mkonev2020@gmail.com>
2021-07-12 14:47:14 +0600

    back-bug #41 copy docker configuration files and and edit the code for docker configuration

--- src/index.js
+++ src/index.js
@@ -1,34 +1,39 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const config = require('../config');
 const app = express();
-const dotenv = require('dotenv');
-dotenv.config();
-
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./swagger/openapi.json');
 
 const locations = require('./api/v1/routes/locations');
 const users = require('./api/v1/routes/users');
 const modules = require('./api/v1/routes/modules');
 const reviews = require('./api/v1/routes/reviews');
 
+const fixApply = require('../fixtures-code');
+
 const { MONGO_HOSTNAME, MONGO_PORT, MONGO_DB, PORT } = process.env;
 
+let url;
 let portVar;
-if (MONGO_HOSTNAME) portVar = PORT;
-else portVar = 85;
 
-const url = `mongodb://${MONGO_HOSTNAME ?? 'localhost'}:${MONGO_PORT}/${MONGO_DB}`;
+if (MONGO_HOSTNAME) {
+  url = `mongodb://${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}`;
+  portVar = PORT;
+} else {
+  url = config.getDbUrl();
+  portVar = 8000;
+}
 
 const run = async () => {
-  await mongoose.connect(url, {
+  mongoose.connect(url, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
     validateBeforeSave: false,
     useCreateIndex: true,
   });
+
+  if (MONGO_HOSTNAME) fixApply(mongoose.connection);
 
   app.use(cors());
   app.use(express.json());
@@ -39,10 +44,11 @@ const run = async () => {
   app.use('/modules', modules());
   app.use('/reviews', reviews());
 
-  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
   app.listen(portVar, () => {
-    console.log('Server started at http://localhost' + portVar);
+    console.log(`Server started at http://localhost ${portVar}`);
   });
 };
 
-run().catch(e => console.log(e));
+run().catch(e => {
+  console.log(e);
+});
