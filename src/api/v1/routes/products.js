bc0f5e077d3a7a7ce85ef549dcbf5838dc2b9cbd
Mikhail Konev <mkonev2020@gmail.com>
2021-08-11 17:26:28 +0600

    back-enhancement #111 add route for side filtering 217 front tiket

--- src/api/v1/routes/products.js
+++ src/api/v1/routes/products.js
@@ -51,6 +51,80 @@ const createRouter = () => {
     }
   });
 
+  router.get('/filters/:queryStr', async (req, res) => {
+    const getOptionValue = (option, key) => {
+      const priceSort = { priceAsc: 'asc', priceDesc: 'desc' };
+      const roominessFilter = { roominess1: '1', roominess2: '2', roominess3: '3', roominess4: '4', roominess5: '5' };
+      const seasonFilter = { seasonSummer: 'summer', seasonWinter: 'winter', seasonAll: 'all' };
+      switch (option) {
+        case 'price':
+          return priceSort[`${key}`];
+        case 'roominess':
+          return roominessFilter[`${key}`];
+        case 'season':
+          return seasonFilter[`${key}`];
+        case 'factory':
+          return key.slice(key.indexOf('_') + 1);
+        case 'category':
+          return key.slice(key.indexOf('_') + 1);
+        default:
+          break;
+      }
+    };
+
+    const optionsArr = ['price', 'roominess', 'season', 'factory', 'category'];
+    const queryArr = JSON.parse(req.params.queryStr);
+
+    try {
+      let prevFilterResult = await Product.find({});
+      let currentFiltered = [];
+      let totalFiltered = [];
+
+      optionsArr
+        .filter(option => option !== 'price')
+        .forEach(option => {
+          let key;
+          if (option === 'category' || option === 'factory') {
+            key = option + 'Id';
+          } else {
+            key = option;
+          }
+
+          const queryFilteredArr = queryArr.filter(item => item.includes(option));
+
+          if (queryFilteredArr.length > 0) {
+            totalFiltered = [];
+            queryFilteredArr.forEach(item => {
+              currentFiltered = filterArr(prevFilterResult, key, getOptionValue(option, item));
+              totalFiltered = totalFiltered.concat(currentFiltered);
+            });
+            prevFilterResult = [...totalFiltered];
+          }
+        });
+
+      optionsArr
+        .filter(option => option === 'price')
+        .forEach(option => {
+          totalFiltered = [...prevFilterResult];
+          const queryFilteredArr = queryArr.filter(item => item.includes(option));
+          if (queryFilteredArr.length > 0) {
+            queryFilteredArr.forEach(item => {
+              if (getOptionValue(option, item) === 'asc') {
+                totalFiltered = sortArrAsc(totalFiltered, 'price');
+              } else {
+                totalFiltered = sortArrDesc(totalFiltered, 'price');
+              }
+            });
+          }
+        });
+
+      res.send(totalFiltered);
+    } catch (error) {
+      console.log(error);
+      res.status(500).send(error);
+    }
+  });
+
   router.get('/pages', (req, res) => {
     let page = parseInt(req.query.page) || 0;
     let limit = parseInt(req.query.limit) || 3;
@@ -97,10 +171,12 @@ const createRouter = () => {
   router.get('/:id', async (req, res) => {
     try {
       const product = await Product.findById(req.params.id);
-      const reviews = await Review.find({product: req.params.id});
+      const reviews = await Review.find({ product: req.params.id });
       const quantity = reviews.length;
-      const sumRating = reviews.reduce(function(a, b) {return a+b.rating}, 0);
-      const average = Math.round(sumRating/quantity);
+      const sumRating = reviews.reduce(function (a, b) {
+        return a + b.rating;
+      }, 0);
+      const average = Math.round(sumRating / quantity);
       product.rating = average;
       product.reviewsQuantity = quantity;
       res.send(product);
