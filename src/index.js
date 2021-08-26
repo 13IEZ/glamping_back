f26cc700b6a8b8f8011a1ebf5a59de3a17f5ef72
Diana Isseyeva <cherepashka.d74@gmail.com>
2021-08-27 03:48:01 +0600

    back-bug #149 changed post request for locations

--- src/index.js
+++ src/index.js
@@ -43,7 +43,7 @@ const run = async () => {
   if (MONGO_HOSTNAME) fixApply(mongoose.connection);
 
   app.use(cors());
-  app.use(express.json());
+  app.use(express.json({limit: '50mb'}));
   app.use(express.static('public'));
 
   app.use('/reservations', reservations());
